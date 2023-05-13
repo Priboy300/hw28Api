4cd2371e287b255646738cd8df80e1479ccb9aa3
Priboy300 <priboy3000@gmail.com>
2023-05-13 23:09:36 +0300

    add steps

--- src/main/java/api/steps/ReqresSteps.java
+++ src/main/java/api/steps/ReqresSteps.java
@@ -3,6 +3,7 @@ package api.steps;
 import api.models.RegisterUserPayload;
 import api.models.SingleUserResponse;
 import api.models.UpdateSingleUser;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 
@@ -11,7 +12,7 @@ import static api.steps.Specification.config;
 
 public class ReqresSteps {
 
-
+    @Step("Отправить запрос GET reqres.in/api/users/id с параметром id = {0}")
     public static SingleUserResponse getSingleUserSuccess(String user) {
 
         return given()
@@ -23,13 +24,13 @@ public class ReqresSteps {
                 .extract().body().jsonPath().getObject(".", SingleUserResponse.class);
 
     }
-
+    @Step("Проверка того, что окончание поля avatar id-image.jpg включает в себя правильный id пользователя.")
     public void checkSingleUserAvatar(SingleUserResponse response) {
         String end_avatar = String.format("%s-image.jpg", response.getData().getId());
         Assertions.assertTrue(response.getData().getAvatar().endsWith(end_avatar),"Поле avatar содержит неверный id");
     }
 
-
+    @Step("Отправить запрос GET reqres.in/api/users/id с параметром id = {0}")
     public static Response getSingleUserUnsuccess(String user) {
 
         return given()
@@ -40,12 +41,12 @@ public class ReqresSteps {
                 .spec(Specification.getResponseSpec(404))
                 .extract().response();
     }
-
+    @Step("Проверка того, что запрос пользователя с несуществующим id правильно обработался")
     public void checkSingleUserBody(Response response) {
         Assertions.assertEquals(response.getBody().asString(), "{}","Поле body несуществующего пользователя содержит информацию");
     }
 
-
+    @Step("Отправить запрос POST reqres.in/api/register с параметром payload = {0}")
     public static Response postUserSuccess(RegisterUserPayload payload) {
 
         return given()
@@ -58,13 +59,13 @@ public class ReqresSteps {
                 .extract()
                 .response();
     }
-
+    @Step("Проверка того, что зарегистрированному пользователю присвоились уникальные id и token")
     public void checkRegisterUser(Response response) {
         Assertions.assertNotEquals(response.jsonPath().get("id"), Integer.valueOf(0),"Id пользователя равен 0");
         Assertions.assertNotEquals(response.jsonPath().get("token"), " ", "Отсутствует token пользователя");
     }
 
-
+    @Step("Отправить запрос POST reqres.in/api/register с параметром payload = {0}")
     public static Response postUserUnSuccess(RegisterUserPayload payload) {
 
         return given()
@@ -77,12 +78,12 @@ public class ReqresSteps {
                 .extract()
                 .response();
     }
-
+    @Step("Проверка того, что вывилось корректное сообщение об отказе в регистрации пользователю с неккоректными данными")
     public void checkUnregisterUser(Response response) {
         Assertions.assertEquals(response.jsonPath().get("error"), "Note: Only defined users succeed registration", "Несуществующий пользователь был найден");
     }
 
-
+    @Step("Отправить запрос DELETE reqres.in/api/users/id с параметром id = {0}")
     public static Response deleteUserSuccess(String user) {
         return given()
                 .spec(Specification.getRequestSpec())
@@ -93,7 +94,7 @@ public class ReqresSteps {
                 .extract()
                 .response();
     }
-
+    @Step("Отправить запрос DELETE reqres.in/api/users/id с параметром id = {0}")
     public static Response deleteUserUnsuccess(String user) {
         return given()
                 .spec(Specification.getRequestSpec())
@@ -105,7 +106,7 @@ public class ReqresSteps {
                 .response();
     }
 
-
+    @Step("Отправить запрос PUT reqres.in/api/users/id с параметром id = {0}")
     public static Response putUpdateSuccess(String id, UpdateSingleUser payload) {
 
         return given()
@@ -119,11 +120,13 @@ public class ReqresSteps {
                 .response();
     }
 
+    @Step("Проверка того, что информация о пользователе корректно обновились")
     public void checkUpdateUser(Response response, UpdateSingleUser payload) {
         Assertions.assertEquals(payload.getJob(), response.jsonPath().get("job"), "Поле работа(job) не совпадает");
         Assertions.assertEquals(payload.getName(), response.jsonPath().get("name"), "Поле имя(name) не совпадает");
     }
 
+    @Step("Отправить запрос PUT reqres.in/api/users/id с параметром id = {0}")
     public static Response putUpdateUnsuccess(String id, UpdateSingleUser payload) {
 
         return given()
@@ -137,6 +140,7 @@ public class ReqresSteps {
                 .response();
     }
 
+    @Step("Проверка того, что запрос на обновление несущестующего пользователя корректно обработался")
     public void checkNotUpdateUser(Response response, UpdateSingleUser payload) {
         Assertions.assertEquals(payload.getJob(), response.jsonPath().get("job"), "Поле работа(job) не совпадает");
         Assertions.assertEquals(payload.getName(), response.jsonPath().get("name"), "Поле имя(name) не совпадает");
--- src/test/java/reqres/TestReqres.java
+++ src/test/java/reqres/TestReqres.java
@@ -4,21 +4,30 @@ import api.models.RegisterUserPayload;
 import api.models.SingleUserResponse;
 import api.models.UpdateSingleUser;
 import api.steps.ReqresSteps;
+import io.qameta.allure.*;
 
 import io.restassured.response.Response;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-
+@Epic("Api tests")
+@Feature("Reqres service")
+@Story("Методы для работы с пользователем")
+@Link(name = "Документация сервиса", url = "https://reqres.in/")
+@Owner("Tomas T")
 public class TestReqres {
 
 
     private final ReqresSteps reqres_steps = new ReqresSteps();
 
     @ParameterizedTest
+    @Description("Проверяем, что поле avatar содержит верный id пользователя")
+    @DisplayName("Проверка поля avatar")
+    @Severity(SeverityLevel.NORMAL)
     @ValueSource(strings = {"1", "2"})
     public void successCheckUserAvatar(String user) {
         SingleUserResponse response = reqres_steps.getSingleUserSuccess(user);
@@ -26,6 +35,9 @@ public class TestReqres {
     }
 
     @ParameterizedTest
+    @Description("Проверяем, что запрос информации о несуществущем пользователе правильно обработался")
+    @DisplayName("Проверка несуществующего пользователя")
+    @Severity(SeverityLevel.NORMAL)
     @ValueSource(strings = {"55", "44"})
     public void unsuccessCheckUserBody(String user) {
         Response response = reqres_steps.getSingleUserUnsuccess(user);
@@ -34,6 +46,9 @@ public class TestReqres {
 
 
     @ParameterizedTest
+    @Description("Проверяем, что пользователь с корректными данными зарегистрировался")
+    @DisplayName("Регистрация при передаче корректных данных")
+    @Severity(SeverityLevel.NORMAL)
     @CsvFileSource(resources = "/test-data/register-user-data.csv", numLinesToSkip = 1)
     public void successRegisterUser(String email, String password) {
         RegisterUserPayload payload = new RegisterUserPayload(email, password);
@@ -42,6 +57,9 @@ public class TestReqres {
     }
 
     @ParameterizedTest
+    @Description("Проверяем, что пользователь с некорректными данными не смог зарегистрироваться")
+    @DisplayName("Отказ в регистрации при передаче некорректных данных")
+    @Severity(SeverityLevel.NORMAL)
     @CsvFileSource(resources = "/test-data/unregister-user-data.csv", numLinesToSkip = 1)
     public void unsuccessRegisterUser(String email, String password) {
         RegisterUserPayload payload = new RegisterUserPayload(email, password);
@@ -51,12 +69,18 @@ public class TestReqres {
 
 
     @ParameterizedTest
+    @Description("Проверяем, что запрос на удаление существующего пользователя корректно обработался")
+    @DisplayName("Удаление существующего пользователя")
+    @Severity(SeverityLevel.NORMAL)
     @ValueSource(strings = {"1", "2"})
     public void successDeleteUser(String user) {
         Response response = reqres_steps.deleteUserSuccess(user);
     }
 
     @ParameterizedTest
+    @Description("Проверяем, что запрос на удаление несуществующего пользователь корректно обработался")
+    @DisplayName("Удаление несуществующего пользователя")
+    @Severity(SeverityLevel.NORMAL)
     @ValueSource(strings = {"ddd", "dddd"})
     public void unsuccessDeleteUser(String user) {
         Response response = reqres_steps.deleteUserUnsuccess(user);
@@ -64,6 +88,9 @@ public class TestReqres {
 
 
     @ParameterizedTest
+    @Description("Проверяем, что запрос на обновление информации существующего пользователь корректно обработался")
+    @DisplayName("Обновление информации существующего пользователя")
+    @Severity(SeverityLevel.NORMAL)
     @CsvFileSource(resources = "/test-data/update-user-data.csv", numLinesToSkip = 1)
     public void successUpdateUser(String id, String name, String job) {
         UpdateSingleUser payload = new UpdateSingleUser(name, job);
@@ -72,6 +99,9 @@ public class TestReqres {
     }
 
     @ParameterizedTest
+    @Description("Проверяем, что запрос на обновление информации несуществующего пользователь корректно обработался")
+    @DisplayName("Обновление информации несуществующего пользователя")
+    @Severity(SeverityLevel.NORMAL)
     @CsvFileSource(resources = "/test-data/not-update-user-data.csv", numLinesToSkip = 1)
     public void unsuccessUpdateUser(String id, String name, String job) {
         UpdateSingleUser payload = new UpdateSingleUser(name, job);
